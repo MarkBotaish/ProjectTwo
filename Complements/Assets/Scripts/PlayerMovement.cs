9aa87fca35ff9264eafb31a783211f12fe1cf537
quincy-buchanan <35669413+quincy-buchanan@users.noreply.github.com>
2018-02-12 17:37:52 -0500

    Movement etc
    Movement, Jumping, and Basic Interaction. Will return to.

---
+++ Complements/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public int playerId;
+	private Player player;
+	Rigidbody2D rb;
+	public Transform groundCheck;
+
+	public float speed, jumpSpeed;
+	public bool grounded = false;
+	public LayerMask ground;
+
+	// Use this for initialization
+	void Start () {
+		player = ReInput.players.GetPlayer (playerId);
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckInput ();
+	}
+
+	void FixedUpdate()
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, 0.2f, ground);
+	}
+
+	void CheckInput()
+	{
+		if (player.GetAxis ("Move Horizontal") != 0.0f) {
+			rb.AddForce (new Vector2 (player.GetAxis ("Move Horizontal") * speed, 0));
+		}
+		if (player.GetButtonDown ("Jump") && grounded) {
+			rb.AddForce (transform.up * jumpSpeed);
+			Debug.Log ("Jump");
+		}
+	}
+}
---
+++ Complements/Assets/Scripts/RightArmMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class RightArmMove : MonoBehaviour {
+
+	//needs work
+
+	public int playerId;
+	private Player player;
+	public Transform target;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		player = ReInput.players.GetPlayer (playerId);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		CheckInput ();
+	}
+
+	void CheckInput()
+	{
+		Vector3 zAxis = new Vector3 (0, 0, 1);
+		if (player.GetButton ("MoveRightArm")) {
+			transform.RotateAround (target.position, zAxis, speed);
+		}
+	}
+}
+
