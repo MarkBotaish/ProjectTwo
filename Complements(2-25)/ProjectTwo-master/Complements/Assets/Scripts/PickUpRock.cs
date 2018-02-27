93b5547415924fb36129904532b2e84c81605f03
MarkBotaish <markbotaish@gmail.com>
2018-02-26 20:41:18 -0500

    2.55
    Trying to fix the pickup

--- Complements(2-25)/ProjectTwo-master/Complements/Assets/Scripts/PickUpRock.cs
+++ Complements(2-25)/ProjectTwo-master/Complements/Assets/Scripts/PickUpRock.cs
@@ -7,10 +7,13 @@ public class PickUpRock : MonoBehaviour {
 
 	public int playerId;
 	private Player player;
-	private Transform arm;
+	public Transform arm;
 	Rigidbody2D rb;
 
 	bool canHold = false;
+    bool isHolding = false;
+    bool right = false;
+    bool left = false;
 
 	// Use this for initialization
 	void Start () {
@@ -21,38 +24,56 @@ public class PickUpRock : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if((player.GetButtonDown("Right Grab") || player.GetButtonDown("Left Grab")) && arm != null && canHold){
-			transform.SetParent (arm);
-			rb.bodyType = RigidbodyType2D.Kinematic;
+		if((player.GetButtonDown("Right Object") || player.GetButtonDown("Left Object")) && arm != null && !isHolding){
 			GetComponent<BoxCollider2D> ().isTrigger = true;
+            rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            isHolding = true;
+            print("WHY");
 		}
+        if (isHolding && !player.GetButton("Right Object") && right) {
+            resetCube();
+		}else if (isHolding && !player.GetButton("Left Object") && left) {
+            resetCube();
+		}
+        if (isHolding)
+            gameObject.transform.position = arm.position;
 
-		if (transform.parent.tag == "RGrab" && player.GetButtonDown("Right Grab")) {
-			transform.SetParent (null);
-			rb.bodyType = RigidbodyType2D.Dynamic;
-			GetComponent<BoxCollider2D> ().isTrigger = false;
-		}
-
-		if (transform.parent.tag == "LGrab" && player.GetButtonDown("Left Grab")) {
-			transform.SetParent (null);
-			rb.bodyType = RigidbodyType2D.Dynamic;
-			GetComponent<BoxCollider2D> ().isTrigger = false;
-		}
 	}
 
-	void OnTriggerEnter2D(Collider2D col)
-	{
-		if (col.gameObject.tag == "RGrab" || col.gameObject.tag == "LGrab") {
-			canHold = true;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if ((col.gameObject.tag == "RGrab" || col.gameObject.tag == "LGrab") && !isHolding) {
 			arm = col.transform;
+            print("shit");
+            if (col.gameObject.tag == "RGrab")
+                right = true;
+            else
+                left = true;
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D col)
 	{
-		if (col.gameObject.tag == "RGrab" || col.gameObject.tag == "LGrab") {
-			canHold = false;
+		if ((col.gameObject.tag == "RGrab" || col.gameObject.tag == "LGrab") && !isHolding) {
 			arm = null;
-		}
+            if (col.gameObject.tag == "RGrab")
+                right = false;
+            else
+                left = false;
+        }
 	}
+
+    void resetCube()
+    {
+        arm.DetachChildren();
+        GetComponent<BoxCollider2D>().isTrigger = false;
+        rb.constraints = RigidbodyConstraints2D.None;
+        isHolding = false;
+        arm = null;
+
+        if (right)
+            right = false;
+        else
+            left = false;
+    }
 }
